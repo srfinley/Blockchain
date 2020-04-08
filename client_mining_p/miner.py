e8ffa7e017f5a44d4de374afe2eecae07c05f09a
Samantha Finley <srfinley@uchicago.edu>
2020-04-08 16:22:49 -0400

    miner now prints out coins mined

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -49,6 +49,8 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
+    coins_mined = 0
+
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -61,7 +63,7 @@ if __name__ == '__main__':
             print(r)
             break
 
-        # TODO: Get the block from `data` and use it to look for a new proof
+        # Get the block from `data` and use it to look for a new proof
         new_proof = proof_of_work(data)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
@@ -73,4 +75,6 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data['message'] == "New Block Forged":
+            coins_mined += 1
+            print(coins_mined)
